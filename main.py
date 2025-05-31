765109e33e8af85e8c13e7d9e58d87a860a50411
andrinwinzap <AndrinWinzap@proton.me>
2025-05-31 19:38:32 +0200

    switch to status packets instead of a finished packet

--- main.py
+++ main.py
@@ -4,101 +4,113 @@ from Trajectory import *
 from SerialParser import *
 from Serialization import *
 
-PORT = '/dev/ttyUSB0'
+PORT = '/dev/ttyUSB1'
 BAUD = 115200
 TIMEOUT = 1
 
-PING = 0x01
-HOME = 0x02
-POS  = 0x03
-LOAD_TRAJ = 0x04
-EXEC_TRAJ = 0x05
-FINISHED = 0x06
-ACK  = 0xEE
-NACK = 0xFF
+class CommandByte:
+    PING = 0x01
+    HOME = 0x02
+    POS  = 0x03
+    LOAD_TRAJ = 0x04
+    EXEC_TRAJ = 0x05
+    FINISHED = 0x06
+    STATUS = 0x07
+    ACK  = 0xEE
+    NACK = 0xFF
+
+class StatusByte:
+    IDLE = 0x01
+    HOMING = 0x02
+    EXECUTING_TRAJ = 0x03
 
 ser = serial.Serial(PORT, BAUD, timeout=0.1)
 com = SerialProtocol(ser)
 
 def pos():
-    com.send_packet(POS)
+    com.send_packet(CommandByte.POS)
     start = time.time()
-    while not com.available():
+    while True:
         if time.time() - start > TIMEOUT:
             return False
-    cmd = com.read()
+        if com.available():
+            cmd = com.read()
+            if cmd.cmd == bytes([CommandByte.POS]):
+                break
     return read_float_le(cmd.payload)
 
 def ping():
-    com.send_packet(PING)
+    com.send_packet(CommandByte.PING)
     while not com.available():
         start = time.time()
         if time.time() - start > TIMEOUT:
             return False
     cmd = com.read()
-    if cmd.cmd == bytes([ACK]):
+    if cmd.cmd == bytes([CommandByte.ACK]):
         return True
     else:
         return False
     
 def home():
-    com.send_packet(HOME)
+    com.send_packet(CommandByte.HOME)
     start = time.time()
     while not com.available():
         if time.time() - start > TIMEOUT:
             return False
     cmd = com.read()
-    if cmd.cmd != bytes([ACK]):
+    if cmd.cmd != bytes([CommandByte.ACK]):
         print("ACK not received")
         return False
     print("ACKED")
-    while not com.available():
-        pass
-    cmd = com.read()
-    if cmd.cmd == bytes([FINISHED]):
-        return True
-    else:
-        return False
+    while True:
+        com.send_packet(CommandByte.STATUS)
+        start = time.time()
+        while not com.available():
+            if time.time() - start > TIMEOUT:
+                return False
+        cmd = com.read()
+        if cmd.cmd == bytes([CommandByte.STATUS]):
+            if cmd.payload[0] == StatusByte.IDLE:
+                return True
     
     
 def load_traj(trajectory: Trajectory):
-    com.send_packet(LOAD_TRAJ, trajectory.serialize())
+    com.send_packet(CommandByte.LOAD_TRAJ, trajectory.serialize())
     start = time.time()
     while not com.available():
         if time.time() - start > TIMEOUT:
             return False
     cmd = com.read()
-    if cmd.cmd == bytes([ACK]):
+    if cmd.cmd == bytes([CommandByte.ACK]):
         return True
     else:
         return False
     
 def exec_traj():
-    com.send_packet(EXEC_TRAJ)
+    com.send_packet(CommandByte.EXEC_TRAJ)
     start = time.time()
     while not com.available():
         if time.time() - start > TIMEOUT:
             return False
     cmd = com.read()
-    if cmd.cmd == bytes([ACK]):
-        return True
-    else:
+    if cmd.cmd != bytes([CommandByte.ACK]):
+        print("ACK not received")
         return False
+    print("ACKED")
+    while True:
+        com.send_packet(CommandByte.STATUS)
+        start = time.time()
+        while not com.available():
+            if time.time() - start > TIMEOUT:
+                return False
+        cmd = com.read()
+        if cmd.cmd == bytes([CommandByte.STATUS]):
+            print(cmd.payload[0])
+            if cmd.payload[0] == StatusByte.IDLE:
+                return True
     
-# print(pos())
-# print(ping())
-
-# wps = []
-# for i in range(64):
-#     wps.append(Waypoint(position=i*0.01, velocity=i*0.02, timestamp=i*20))
-# trajectory = Trajectory(wps)
-
-# print(load_traj(trajectory))
-# print(exec_traj())
-
-
-print(home())   
+print(home())
 wps = [Waypoint(0, 0, 0), Waypoint(3.14/2, 0, 5000), Waypoint(0, 0, 10000)]
 traj = Trajectory(wps)
-load_traj(traj)
-exec_traj()
+print(load_traj(traj))
+print(exec_traj())
